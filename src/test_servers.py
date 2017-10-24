edcd555d08615a9f378f830b026f3ab987b52f5f
nosrac77 <carson.newton@outlook.com>
2017-10-24 13:57:55 -0700

    created 4 more tests

--- src/test_servers.py
+++ src/test_servers.py
@@ -13,7 +13,7 @@ def test_server():
 def test_response_ok_returns_response():
     """Function that tests reponse_ok function returns response."""
     from server import response_ok
-    assert response_ok().contains('HTTP')
+    assert response_ok().contains('200')
 
 
 def test_output_type_of_response_ok():
@@ -34,3 +34,29 @@ def test_if_response_ok_needs_no_inputs():
     import pytest
     with pytest.raises(TypeError):
         response_ok(5)
+
+
+def test_response_error_returns_response():
+    """Function that tests reponse_error function returns response."""
+    from server import response_error
+    assert response_error().contains('500')
+
+
+def test_output_type_of_response_error():
+    """Test that the output is the correct type."""
+    from server import response_error
+    assert response_error() == str
+
+
+def test_output_of_response_error_exists():
+    """Test that the output exists."""
+    from server import response_error
+    assert response_error() is not None
+
+
+def test_if_response_error_needs_no_inputs():
+    """Test that an unnecessary parameter will raise a TypeError."""
+    from server import response_error
+    import pytest
+    with pytest.raises(TypeError):
+        response_error(5)
