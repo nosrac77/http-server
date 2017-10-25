bc2b84c6f25066647710af3f38cce02ca8b46675
nosrac77 <carson.newton@outlook.com>
2017-10-25 15:00:44 -0700

    created response_request function

--- src/server.py
+++ src/server.py
@@ -41,10 +41,13 @@ def response_ok():
     return response.encode()
 
 
-def response_error():
+def response_error(error):
     """Send back an HTTP 500 response."""
     import datetime
-    response = "HTTP/1.1 500 Internal Server Error\nDate: {}\n\r\n\nYour message was received.".format(datetime.datetime.now())
+    if error is TypeError:
+        response = "HTTP/1.1 400 Bad Request\nDate: {}\n\r\n\nYour message was received.".format(datetime.datetime.now())
+    else:
+        response = "HTTP/1.1 500 Internal Server Error\nDate: {}\n\r\n\nYour message was received.".format(datetime.datetime.now())
     return response.encode()
 
 
@@ -52,10 +55,13 @@ def parse_request(request):
     """Function that returns URI from client if conditions are met."""
     request = request.decode('utf8')
     if 'GET' != request[:3]:
+        response_error(TypeError)
         raise(TypeError)
     if request.split()[2].split('\r\n')[0] != 'HTTP/1.1':
+        response_error(TypeError)
         raise(TypeError)
     if request.split('\r\n')[1][:6] != 'Host: ':
+        response_error(TypeError)
         raise(TypeError)
     return request.split()[1]
 
