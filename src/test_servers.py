4663c994c083467718836ce9f35714c5f9912e86
Nathan Moore <ncmoore1986@gmail.com>
2017-10-25 15:09:33 -0700

    added 3 test for response_error and deleted obsolete ones

--- src/test_servers.py
+++ src/test_servers.py
@@ -27,32 +27,6 @@ def test_if_response_ok_needs_no_inputs():
         response_ok(5)
 
 
-def test_response_error_returns_response():
-    """Function that tests reponse_error function returns response."""
-    from server import response_error
-    assert b"500" in response_error()
-
-
-def test_output_type_of_response_error():
-    """Test that the output is the correct type."""
-    from server import response_error
-    assert isinstance(response_error(), bytes)
-
-
-def test_output_of_response_error_exists():
-    """Test that the output exists."""
-    from server import response_error
-    assert response_error() is not None
-
-
-def test_if_response_error_needs_no_inputs():
-    """Test that an unnecessary parameter will raise a TypeError."""
-    from server import response_error
-    import pytest
-    with pytest.raises(TypeError):
-        response_error(5)
-
-
 def test_parse_request_correct_input():
     """Ensure that parse_request returns the URI given a correct input."""
     from server import parse_request
@@ -66,3 +40,23 @@ def test_parse_request_incorrect_input():
     import pytest
     with pytest.raises(TypeError):
         parse_request(b'bingbong')
+
+
+def test_output_type_of_response_error():
+    """Test that the output is the correct type."""
+    from server import response_error
+    assert type(response_error(TypeError)) == bytes
+
+
+def test_output_of_response_error_exists():
+    """Test that the output exists."""
+    from server import response_error
+    assert response_error(TypeError) is not None
+
+
+def test_if_response_error_needs_inputs():
+    """Test that the inputs are required."""
+    from server import response_error
+    import pytest
+    with pytest.raises(TypeError):
+        response_error()
