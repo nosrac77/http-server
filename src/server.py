8460ab38ff690ed86231e108a1f4481e0166d081
nosrac77 <carson.newton@outlook.com>
2017-10-25 15:28:20 -0700

    attempted to send response to client, attempts failed.

--- src/server.py
+++ src/server.py
@@ -3,6 +3,35 @@
 import socket
 
 
+def response_ok():
+    """Send back an HTTP 200 response."""
+    import datetime
+    response = "HTTP/1.1 200 OK\nDate: {}\n\r\n\nYour message was received.".format(datetime.datetime.now())
+    return response.encode()
+
+
+def response_error(error):
+    """Send back an HTTP 500 response."""
+    import datetime
+    if error is TypeError:
+        response = "HTTP/1.1 400 Bad Request\nDate: {}\n\r\n\nYour message was received.".format(datetime.datetime.now())
+    else:
+        response = "HTTP/1.1 500 Internal Server Error\nDate: {}\n\r\n\nYour message was received.".format(datetime.datetime.now())
+        return response.encode()
+
+
+def parse_request(request):
+    """Function that returns URI from client if conditions are met."""
+    request = request.decode('utf8')
+    if 'GET' != request[:3]:
+        raise(TypeError)
+    if request.split()[2].split('\r\n')[0] != 'HTTP/1.1':
+        raise(TypeError)
+    if request.split('\r\n')[1][:6] != 'Host: ':
+        raise(TypeError)
+    return request.split()[1]
+
+
 def start_server():
     """Start the server."""
     import sys
@@ -26,44 +55,17 @@ def start_server():
                 if len(part) < buffer_length:
                     timer = False
 
-            conn.sendall(entire_message)
             conn.close()
+            try:
+                conn.sendall(response_ok().encode())
+            except TypeError:
+                conn.sendall(response_error(TypeError))
 
     except KeyboardInterrupt:
         server.close()
         sys.exit(1)
 
 
-def response_ok():
-    """Send back an HTTP 200 response."""
-    import datetime
-    response = "HTTP/1.1 200 OK\nDate: {}\n\r\n\nYour message was received.".format(datetime.datetime.now())
-    return response.encode()
-
-
-def response_error(error):
-    """Send back an HTTP 500 response."""
-    import datetime
-    if error is TypeError:
-        response = "HTTP/1.1 400 Bad Request\nDate: {}\n\r\n\nYour message was received.".format(datetime.datetime.now())
-    else:
-        response = "HTTP/1.1 500 Internal Server Error\nDate: {}\n\r\n\nYour message was received.".format(datetime.datetime.now())
-    return response.encode()
-
-
-def parse_request(request):
-    """Function that returns URI from client if conditions are met."""
-    request = request.decode('utf8')
-    if 'GET' != request[:3]:
-        response_error(TypeError)
-        raise(TypeError)
-    if request.split()[2].split('\r\n')[0] != 'HTTP/1.1':
-        response_error(TypeError)
-        raise(TypeError)
-    if request.split('\r\n')[1][:6] != 'Host: ':
-        response_error(TypeError)
-        raise(TypeError)
-    return request.split()[1]
 
 
 if __name__ == "__main__":
