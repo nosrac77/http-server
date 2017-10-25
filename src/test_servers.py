519815e7ef8a78b0744c5075193d14d0a5b5c183
Nathan Moore <ncmoore1986@gmail.com>
2017-10-25 14:13:51 -0700

    wrote two tests for parse_requests

--- src/test_servers.py
+++ src/test_servers.py
@@ -60,3 +60,18 @@ def test_if_response_error_needs_no_inputs():
     import pytest
     with pytest.raises(TypeError):
         response_error(5)
+
+
+def test_parse_request_correct_input():
+    """Ensure that parse_request returns the URI given a correct input."""
+    from server import parse_request
+    good_input = "GET /path/to/index.html HTTP/1.1<CRLF>Host: www.mysite1.com:80<CRLF><CRLF>"
+    assert parse_request(good_input) == '/path/to/index.html'
+
+
+def test_parse_request_incorrect_input():
+    """Ensure that parse_request raises a TypeError given an incorrect input."""
+    from server import parse_request
+    import pytest
+    with pytest.raises(TypeError):
+        parse_request('bingbong')
