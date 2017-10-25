637f0644c6b7066dab5d81a5feaefbbf6ccf2e6b
Nathan Moore <ncmoore1986@gmail.com>
2017-10-25 13:24:18 -0700

    fixed whitespace

--- src/client.py
+++ src/client.py
@@ -5,33 +5,23 @@ def start_client(message):
     """Start the client."""
     import socket
     client_socket = socket.socket(2, 1, 6)
-    client_socket.connect(('127.0.0.1', 5679))
+    client_socket.connect(('127.0.0.1', 5678))
     client_socket.sendall(message.encode('utf8'))
-<<<<<<< HEAD
-
-    buffer_length = 100
-=======
     buffer_length = 8
->>>>>>> eb664115b48dff6ed7a973c996b4819b75f70c90
-
     entire_message = ''
     timer = True
+
     while timer:
         part = client_socket.recv(buffer_length)
         print(part.decode('utf8'))
         entire_message += part
+
         if len(part) < buffer_length:
-<<<<<<< HEAD
-            break
-    client_socket.shutdown(socket.SHUT_WR)
-=======
             timer = False
 
->>>>>>> eb664115b48dff6ed7a973c996b4819b75f70c90
     client_socket.close()
 
 
 if __name__ == "__main__":
     import sys
     start_client(sys.argv[1])
-    sys.exit()
--- src/server.py
+++ src/server.py
@@ -6,48 +6,29 @@ import socket
 def start_server():
     """Start the server."""
     import sys
+
     server = socket.socket(2, 1, 6)
-<<<<<<< HEAD
-    server.bind(("127.0.0.1", 5679))
-
-    server.listen(20)
-
-    buffer_length = 100
-
-    message_complete = False
-
-    entire_message = ""
-
-    while not message_complete:
-        conn, addr = server.accept()
-        part = conn.recv(buffer_length)
-        entire_message += part.decode('utf8')
-        if len(part) < buffer_length:
-            break
-
-    print(entire_message)
-
-    conn.sendall(response_ok())
-
-=======
     server.bind(("127.0.0.1", 5678))
     server.listen(20)
     buffer_length = 8
->>>>>>> eb664115b48dff6ed7a973c996b4819b75f70c90
+
     try:
         while True:
             conn, addr = server.accept()
             entire_message = ''
             timer = True
+
             while timer:
                 part = conn.recv(buffer_length)
                 print(part.decode('utf8'))
                 entire_message += part.decode('utf8')
+
                 if len(part) < buffer_length:
                     timer = False
 
             conn.sendall(entire_message)
             conn.close()
+
     except KeyboardInterrupt:
         server.close()
         sys.exit(1)
@@ -57,7 +38,6 @@ def response_ok():
     """Send back an HTTP 200 response."""
     import datetime
     response = "HTTP/1.1 200 OK\nDate: {}\n<CRLF>\nYour message was received.".format(datetime.datetime.now())
-
     return response.encode()
 
 
@@ -65,7 +45,6 @@ def response_error():
     """Send back an HTTP 500 response."""
     import datetime
     response = "HTTP/1.1 500 Internal Server Error\nDate: {}\n<CRLF>\nYour message was received.".format(datetime.datetime.now())
-
     return response.encode()
 
 
