4647e2f5e17c986595f7b6442897dad92533df42
nosrac77 <carson.newton@outlook.com>
2017-10-26 13:49:31 -0700

    error responses and 200 ok responses are being sent correctly

--- src/server.py
+++ src/server.py
@@ -13,7 +13,6 @@ def response_ok():
 def response_error(error):
     """Send back an HTTP 500 response."""
     import datetime
-    print(error)
     if error is TypeError:
         response = "HTTP/1.1 400 Bad Request\nDate: {}\n\r\n\nYour message was received.".format(datetime.datetime.now())
     else:
@@ -25,9 +24,9 @@ def parse_request(request):
     """Function that returns URI from client if conditions are met."""
     if 'GET' != request[:3]:
         raise(TypeError)
-    if request.split()[2].split('\r\n')[0] != 'HTTP/1.1':
+    if request.split()[2][:8] != 'HTTP/1.1':
         raise(TypeError)
-    if request.split('\r\n')[1][:6] != 'Host: ':
+    if request.split()[2][12:18] != 'Host:':
         raise(TypeError)
     return request.split()[1]
 
