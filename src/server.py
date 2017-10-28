c8c5cc7db7289cfc38552446bad3428494f1ea0a
nosrac77 <carson.newton@outlook.com>
2017-10-27 18:57:44 -0700

    server now stays open properly and returns 200 ok response

--- src/server.py
+++ src/server.py
@@ -3,57 +3,52 @@
 import socket
 
 
-def start_server():
-    """Start the server."""
-    server = socket.socket(2, 1, 6)
-    server.bind(("127.0.0.1", 5679))
-
-    server.listen(20)
-
-
-    buffer_length = 8
-
-    message_complete = False
-
-    entire_message = ""
-
-    while not message_complete:
-        conn, addr = server.accept()
-        part = conn.recv(buffer_length)
-        entire_message += part.decode('utf8')
-        if len(part) < buffer_length:
-            break
-
-    print(entire_message)
-
-    conn.sendall(response_ok())
-
-    try:
-        i = input('Press ctrl-d to exit.')
-        if i == KeyboardInterrupt:
-            conn.close()
-            server.close()
-        else:
-            conn.sendall(entire_message.decode('UTF8'))
-    except EOFError:
-        conn.close()
-        server.close()
-
-
 def response_ok():
     """Send back an HTTP 200 response."""
     import datetime
-    response = "HTTP/1.1 200 OK\nDate: {}\n<CRLF>\nYour message was received.".format(datetime.datetime.now())
-
+    response = "HTTP/1.1 200 OK\nDate: {}\n\r\n\nYour message was received.".format(datetime.datetime.now())
     return response.encode()
 
 
-def response_error():
+def response_error(error):
     """Send back an HTTP 500 response."""
     import datetime
-    response = "HTTP/1.1 500 Internal Server Error\nDate: {}\n<CRLF>\nYour message was received.".format(datetime.datetime.now())
-
+    if error is TypeError:
+        response = "HTTP/1.1 400 Bad Request\nDate: {}\n\r\n\nYour message was received.".format(datetime.datetime.now())
+    else:
+        response = "HTTP/1.1 500 Internal Server Error\nDate: {}\n\r\n\nYour message was received.".format(datetime.datetime.now())
     return response.encode()
+
+
+def start_server():
+    """Start the server."""
+    import sys
+
+    server = socket.socket(2, 1, 6)
+    server.bind(("127.0.0.1", 5679))
+    server.listen(20)
+    buffer_length = 8
+
+    try:
+        while True:
+            conn, addr = server.accept()
+            entire_message = ''
+            timer = True
+
+            while timer:
+                part = conn.recv(buffer_length)
+                print(part.decode('utf8'))
+                entire_message += part.decode('utf8')
+
+                if len(part) < buffer_length:
+                    timer = False
+
+                conn.sendall(response_ok())
+                conn.sendall(entire_message.encode())
+
+    except KeyboardInterrupt:
+        server.close()
+        sys.exit(1)
 
 
 if __name__ == "__main__":
