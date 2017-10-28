5956570a4e52ebfef0cc74d0f534efecb87b23a7
nosrac77 <carson.newton@outlook.com>
2017-10-27 18:49:11 -0700

    also refactored clientside code and tested to ensure functionality

--- src/client.py
+++ src/client.py
@@ -4,17 +4,19 @@
 def start_client(message):
     """Start the client."""
     import socket
+
     client_socket = socket.socket(2, 1, 6)
     client_socket.connect(('127.0.0.1', 5678))
-    client_socket.sendall(message.encode('utf8'))
+    client_socket.sendall(message)
     buffer_length = 8
-
-    entire_message = ''
+    entire_message = b''
     timer = True
+
     while timer:
         part = client_socket.recv(buffer_length)
         print(part.decode('utf8'))
         entire_message += part
+
         if len(part) < buffer_length:
             timer = False
 
