59c13b76ffd73760ba78a39a7bb4ea6b53064bdf
nosrac77 <carson.newton@outlook.com>
2017-10-27 18:43:18 -0700

    refactored to keep server open

--- src/server.py
+++ src/server.py
@@ -1,29 +1,28 @@
-"""Function that creates a server to interact with client.py."""
-
-import socket
-
-
 def start_server():
     """Start the server."""
     import sys
+
     server = socket.socket(2, 1, 6)
     server.bind(("127.0.0.1", 5678))
     server.listen(20)
     buffer_length = 8
+
     try:
         while True:
             conn, addr = server.accept()
             entire_message = ''
             timer = True
+
             while timer:
                 part = conn.recv(buffer_length)
                 print(part.decode('utf8'))
                 entire_message += part.decode('utf8')
+
                 if len(part) < buffer_length:
                     timer = False
 
-            conn.sendall(entire_message)
-            conn.close()
+                conn.sendall(entire_message)
+
     except KeyboardInterrupt:
         server.close()
         sys.exit(1)
